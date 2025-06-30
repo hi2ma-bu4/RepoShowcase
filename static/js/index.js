6ebc68ebcfcab573b257694a794efff5489c7adc
hi2ma-bu4 <hi2ma.bu4.bot@gmail.com>
2025-06-30 23:46:29 +0900

    🐛 url修正

--- static/js/index.js
+++ static/js/index.js
@@ -2,14 +2,17 @@ const libraries = [
 	{
 		name: "JavaLibraryScript",
 		description: "JavaScriptでJavaっぽい機能を提供するライブラリ。",
+		github: "JavaLibraryScript",
 	},
 	{
 		name: "WinLet",
 		description: "ウィンドウUIライブラリ。",
+		github: "winlet",
 	},
 	{
 		name: "SnowFall",
 		description: "独自言語『SnowFall』のコンパイル、実行環境を提供する。",
+		github: "SnowFall",
 	},
 ];
 
@@ -24,8 +27,8 @@ jasc.on("DOMContentLoaded", () => {
 		<h2>${lib.name}</h2>
 		<p>${lib.description}</p>
 		<div class="card-links">
-			<a href="https://github.com/hi2ma-bu4/${lib.name}" target="_blank">GitHub</a>
-			<a href="https://hi2ma-bu4.github.io/RepoShowcase/${lib.name}/test/" target="_blank">デモ</a>
+			<a href="https://github.com/hi2ma-bu4/${lib.github}" target="_blank">GitHub</a>
+			<a href="https://hi2ma-bu4.github.io/RepoShowcase/public/${lib.github.toLowerCase()}/test/">デモ</a>
 		</div>
 	`;
 
